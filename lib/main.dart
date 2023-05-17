98d02fe00f4f88b6bb9e0df2e6c01ddf1150369b
idhpaul <idhpaul@naver.com>
2023-05-17 13:50:16 +0900

    develope token manager

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,13 @@ void main() {
   runApp(const MyApp());
 }
 
+class BottomNavigationBarModel {
+  IconData icon;
+  String label;
+
+  BottomNavigationBarModel({required this.icon, required this.label});
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -42,15 +49,249 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<BottomNavigationBarModel> bottomNavigationBarItems = [
+    BottomNavigationBarModel(icon: Icons.home, label: "Home"),
+    BottomNavigationBarModel(icon: Icons.account_circle, label: "Account")
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-      child:Column(
-              mainAxisAlignment: MainAxisAlignment.center, 
-              children: const [
-                DolbyClient(),
-      ]),
-    ));
+        backgroundColor: const Color(0xffffffff),
+        appBar: AppBar(
+          elevation: 4,
+          centerTitle: false,
+          automaticallyImplyLeading: false,
+          backgroundColor: const Color(0xff3a57e8),
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.zero,
+          ),
+          title: const Text(
+            "AppBar",
+            style: TextStyle(
+              fontWeight: FontWeight.w400,
+              fontStyle: FontStyle.normal,
+              fontSize: 14,
+              color: Color(0xff000000),
+            ),
+          ),
+          leading: const Icon(
+            Icons.arrow_back,
+            color: Color(0xff212435),
+            size: 24,
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: bottomNavigationBarItems.map((BottomNavigationBarModel item) {
+            return BottomNavigationBarItem(
+              icon: Icon(item.icon),
+              label: item.label,
+            );
+          }).toList(),
+          backgroundColor: const Color(0xffffffff),
+          currentIndex: 0,
+          elevation: 8,
+          iconSize: 24,
+          selectedItemColor: const Color(0xff3a57e8),
+          unselectedItemColor: const Color(0xff9e9e9e),
+          selectedFontSize: 14,
+          unselectedFontSize: 14,
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          onTap: (value) {},
+        ),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  const Text(
+                    "토큰 상태",
+                    textAlign: TextAlign.start,
+                    overflow: TextOverflow.clip,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                      fontSize: 14,
+                      color: Color(0xff000000),
+                    ),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.add),
+                    onPressed: () {},
+                    color: const Color(0xff212435),
+                    iconSize: 24,
+                  ),
+                ],
+              ),
+              const Divider(
+                color: Color(0xff808080),
+                height: 16,
+                thickness: 0,
+                indent: 0,
+                endIndent: 0,
+              ),
+              Container(
+                margin: const EdgeInsets.all(0),
+                padding: const EdgeInsets.all(10),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: const Color(0x1f000000),
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(10.0),
+                  border: Border.all(color: const Color(0x4d9e9e9e), width: 1),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Expanded(
+                      flex: 4,
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 5, 0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            const Text(
+                              "Text",
+                              textAlign: TextAlign.start,
+                              overflow: TextOverflow.clip,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontStyle: FontStyle.normal,
+                                fontSize: 20,
+                                color: Color(0xff000000),
+                              ),
+                            ),
+                            TextField(
+                              controller: TextEditingController(),
+                              obscureText: false,
+                              textAlign: TextAlign.left,
+                              maxLines: 1,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontStyle: FontStyle.normal,
+                                fontSize: 14,
+                                color: Color(0xff000000),
+                              ),
+                              decoration: InputDecoration(
+                                disabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(4.0),
+                                  borderSide: const BorderSide(
+                                      color: Color(0xff000000), width: 1),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(4.0),
+                                  borderSide: const BorderSide(
+                                      color: Color(0xff000000), width: 1),
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(4.0),
+                                  borderSide: const BorderSide(
+                                      color: Color(0xff000000), width: 1),
+                                ),
+                                hintText: "Enter Text",
+                                hintStyle: const TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  fontStyle: FontStyle.normal,
+                                  fontSize: 14,
+                                  color: Color(0xff000000),
+                                ),
+                                filled: true,
+                                fillColor: const Color(0xfff2f2f3),
+                                isDense: true,
+                                contentPadding: const EdgeInsets.symmetric(
+                                    vertical: 8, horizontal: 12),
+                              ),
+                            ),
+                            TextField(
+                              controller: TextEditingController(),
+                              obscureText: false,
+                              textAlign: TextAlign.start,
+                              maxLines: 1,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontStyle: FontStyle.normal,
+                                fontSize: 14,
+                                color: Color(0xff000000),
+                              ),
+                              decoration: InputDecoration(
+                                disabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(4.0),
+                                  borderSide: const BorderSide(
+                                      color: Color(0xff000000), width: 1),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(4.0),
+                                  borderSide: const BorderSide(
+                                      color: Color(0xff000000), width: 1),
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(4.0),
+                                  borderSide: const BorderSide(
+                                      color: Color(0xff000000), width: 1),
+                                ),
+                                hintText: "Enter Text",
+                                hintStyle: const TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  fontStyle: FontStyle.normal,
+                                  fontSize: 14,
+                                  color: Color(0xff000000),
+                                ),
+                                filled: true,
+                                fillColor: const Color(0xfff2f2f3),
+                                isDense: true,
+                                contentPadding: const EdgeInsets.symmetric(
+                                    vertical: 8, horizontal: 12),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+                        child: MaterialButton(
+                          onPressed: () {},
+                          color: const Color(0xffffffff),
+                          elevation: 0,
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero,
+                            side:
+                                BorderSide(color: Color(0xff808080), width: 1),
+                          ),
+                          padding: const EdgeInsets.all(16),
+                          textColor: const Color(0xff000000),
+                          height: 40,
+                          minWidth: 140,
+                          child: const Text(
+                            "Button",
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                              fontStyle: FontStyle.normal,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const DolbyClient()
+            ],
+          ),
+        ));
   }
 }
--- lib/src/media_api_handle.dart
+++ lib/src/media_api_handle.dart
@@ -1,22 +1,88 @@
-import 'dart:async';
+// ignore_for_file: constant_identifier_names
+
+import 'dart:convert';
+import 'dart:core';
+import 'dart:io';
 
 import 'package:flutter_mediaapi_client/src/util/env.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:schedulers/schedulers.dart';
+import 'package:http/http.dart';
+
+enum APIReturnType {
+
+  ERROR,
+
+  OK,
+
+  TOKEN_NONE,
+  TOKEN_EXPIRE,
+  TOKEN_VERIFY,
+
+
+}
 
 class MediaAPI {
-  final _authManager = AuthManger()..init();
   final _apiManager = ApiManger();
+  final _tokenManager = TokenManager()..init();
+  final _tokenBagroundService = IntervalScheduler(delay: const Duration(seconds: 10)); 
+  bool hasToken = false;
 
   void write(String key, dynamic value) async {
-    _authManager.write(key, value);
+    _tokenManager.write(key, value);
   }
 
   dynamic read<T>(dynamic key) {
-    return _authManager.read<T>(key);
+    return _tokenManager.read<T>(key);
   }
+
+  void registerTokenBackgroundService(){
+    _tokenBagroundService.run(() => _callbackTokenBackgroundService());
+  }
+
+  void _callbackTokenBackgroundService(){
+    // check token
+    hasToken = (APIReturnType.TOKEN_VERIFY ==_checkToken()) ? true : false;
+    
+  }
+
+
+  APIReturnType _checkToken() {
+    return _tokenManager.checkVerifyToken();
+  }
+
+  void _publishToken() {
+    var retJson = _apiManager.getToken();
+
+    //write('access_token', retJson['access_token']);
+    //write('access_token_expire', expireDateTime.millisecondsSinceEpoch);
+  }
+
+  String _getToken() {
+    return read<String>('access_token');
+  }
+
+  String  getToken() {
+    var ret = _tokenManager.checkVerifyToken();
+    
+    // switch (ret) {
+    //   case APIReturnType.TOKEN_NONE:
+    //     _publishToken();
+    //     break;
+    //   case APIReturnType.TOKEN_EXPIRE:
+    //     _publishToken();
+    //     break;
+    //   case APIReturnType.TOKEN_VERIFY:
+    //     break;
+    // }
+
+    return _getToken();
+    
+  }
+
 }
 
-class AuthManger{
+class TokenManager{
 
   //Storage location by platform 
   //[Android]	      SharedPreferences
@@ -27,10 +93,12 @@ class AuthManger{
   //[Windows]	      In the roaming AppData directory
 
   late SharedPreferences _prefs;
+  bool _hasToken = false;
 
-  Future init() async =>
+  Future init() async {
       _prefs = await SharedPreferences.getInstance();
-
+      _hasToken = (APIReturnType.TOKEN_VERIFY==checkVerifyToken()) ? true : false;
+  }
   //write token
   void write(String key, dynamic value) async {
 
@@ -87,9 +155,89 @@ class AuthManger{
 
   //query token
 
-  //get value from key
+  //get token
+  void getToken(){
+    var storeToken = read<String>('access_token');
+    print(storeToken);
+  }
+
+  void publishToken() {
+
+  }
+
+  //init verify access token
+  APIReturnType checkVerifyToken() {
+    var currentTimeStamp = DateTime.now().millisecondsSinceEpoch;
+    print("Current Timestamp(GST, KST) : $currentTimeStamp, ${(DateTime.fromMillisecondsSinceEpoch(currentTimeStamp).toLocal())}");
+
+    try {
+      var storeTokenExpireTimeStamp = read<int>('access_token_expire');
+      if(storeTokenExpireTimeStamp == null){
+        return APIReturnType.TOKEN_NONE;
+      } else {
+        print("Current Token Timestamp(GST, KST) : $storeTokenExpireTimeStamp, ${DateTime.fromMillisecondsSinceEpoch(storeTokenExpireTimeStamp).toLocal()}");
+      }
+
+      return (currentTimeStamp > storeTokenExpireTimeStamp ) ? APIReturnType.TOKEN_EXPIRE : APIReturnType.TOKEN_VERIFY;
+
+    } catch (e) {
+      print(e);
+      return APIReturnType.ERROR;
+    }
+    
+  }
 }
 
 class ApiManger{
 
+  void getToken() async {
+    String appkey = ENV['DolbyMediaAPIAppKey']!;
+    String appsecret = ENV['DolbyMediaAPIAppSecretKey']!;
+    String basicAuth = 'Basic ${base64.encode(utf8.encode('$appkey:$appsecret'))}';
+    print(basicAuth);
+
+    // ref : https://docs.dolby.io/media-apis/reference/get-api-token
+    //Uri uri = Uri.parse("https://api.dolby.io/v1/auth/token?expires_in=86400");
+    Uri uri = Uri.https("api.dolby.io", "/v1/auth/token", {"grant_type":"client_credentials","expires_in":"86400"});
+    print(uri);
+
+    Map<String, String> header = {
+      'authorization': basicAuth,
+      'content-type': "application/x-www-form-urlencoded",
+    };
+
+    try {
+      final response = await post(
+        uri,
+        headers: header,
+      );
+
+      if (response.statusCode != 200)throw HttpException('${response.statusCode} / ${response.body}');
+
+      logger.i(response.statusCode);
+      logger.i(response.body);
+
+      var decodedResponse = jsonDecode(utf8.decode(response.bodyBytes)) as Map;
+      var expireDateTime = HttpDate.parse(response.headers['date']!).add(Duration(seconds: decodedResponse['expires_in']));
+
+      decodedResponse.addEntries({'access_token_expire': '$expireDateTime'} as Iterable<MapEntry>);
+      print("Token Expire(GST, KST) : ${expireDateTime.millisecondsSinceEpoch}, ${expireDateTime.toLocal()}");
+
+      // var expireTimeStamp = (expireDateTime.millisecondsSinceEpoch + DateTime.now().timeZoneOffset.inMilliseconds); 
+      // print(expireTimeStamp);
+
+      //logger.d(decodedResponse['token_type']);
+      //logger.d(decodedResponse['access_token']);
+
+      return decodedResponse;
+
+    } on SocketException {
+      logger.e('No Internet connection 😑');
+    } on HttpException catch (e) {
+      logger.e("Couldn't find the get 😱/n ${e.message}");
+    } catch (e) {
+      // executed for errors of all types other than Exception
+      logger.e("Couldn't find the get 😱/n ${e}");
+    }
+  }
 }
